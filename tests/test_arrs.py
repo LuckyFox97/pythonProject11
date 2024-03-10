e52fc1cf66bc0f259c50f7053620bdba7fa3452e
Dmitry <dima12_yarrin@mail.ru>
2024-03-10 09:15:36 +0300

    Test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,27 @@
-from utils import arrs
+import unittest
+from utils.arrs import get, my_slice
 
+class TestFunctions(unittest.TestCase):
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    def test_get_existing_index(self):
+        array = [1, 2, 3, 4, 5]
+        self.assertEqual(get(array, 2), 3)
 
+    def test_get_non_existing_index(self):
+        array = [1, 2, 3]
+        self.assertEqual(get(array, 5, 'default'), 'default')
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    def test_my_slice_full_range(self):
+        coll = [1, 2, 3, 4, 5]
+        self.assertEqual(my_slice(coll), coll)
+
+    def test_my_slice_with_start(self):
+        coll = [1, 2, 3, 4, 5]
+        self.assertEqual(my_slice(coll, 1), [2, 3, 4, 5])
+
+    def test_my_slice_with_end(self):
+        coll = [1, 2, 3, 4, 5]
+        self.assertEqual(my_slice(coll, end=3), [1, 2, 3])
+
+if __name__ == '__main__':
+    unittest.main()
